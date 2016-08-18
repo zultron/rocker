e0c5157b8ee2451862aadce3203836d53ce6c7b7
John Morris <john@zultron.com>
2016-08-18 12:28:33 -0500

    docker-dev:  handle preexisting containers  FIXME WIP
    When running `docker-dev`, handle preexisting containers:
    - Not running:  remove container (right now disabled)
    - Running:  exit with error message

--- docker-dev.sh
+++ docker-dev.sh
@@ -17,6 +17,26 @@ fi
 IMAGE=dev-${SUITE}
 NAME=${IMAGE}
 
+# Check for existing containers
+EXISTING="$(docker ps -aq --filter=name=${NAME})"
+if test -n "${EXISTING}"; then
+    # Container exists; is it running?
+    RUNNING=$(docker inspect ${EXISTING} | awk '/"Running":/ { print $2 }')
+    if test "${RUNNING}" = "false,"; then
+	# Remove stopped container
+	echo docker rm ${EXISTING}
+    elif test "${RUNNING}" = "true,"; then
+	# Container already running; error
+	echo "Error:  container '${NAME}' already running" >&2
+	exit 1
+    else
+	# Something went wrong
+	echo "Error:  unable to determine status of " \
+	    "existing container '${EXISTING}'" >&2
+	exit 1
+    fi
+fi
+
 docker run --rm \
     -it --privileged \
     -u `id -u`:`id -g` \
